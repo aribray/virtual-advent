714e2ccb145b40ba811a379b696f8a6a07138f3a
Ariana Bray <aribray@gmail.com>
2020-11-23 14:43:02 -0800

    Wishlist and item card population

--- src/components/item.jsx
+++ src/components/item.jsx
@@ -8,6 +8,7 @@ import { Card,
          CardActions,
          Button }     from '@material-ui/core'
 import { makeStyles } from '@material-ui/styles'
+import { ReactTinyLink } from 'react-tiny-link';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -22,34 +23,59 @@ const useStyles = makeStyles(theme => ({
   }
 }))
 
-const Item = () => {
+const ItemContainer = ({ onEditButtonClick }) => props => {
+  return <Item item={props.item} onEditButtonClick={onEditButtonClick} />
+}
+
+const Item = React.memo((props) => {
+  console.log(props)
   const classes = useStyles()
+
+  let itemURL = ""
+  let description = ""
+
+  if (props.item.description) {
+    description =
+    <div>
+      <h4>Description</h4>
+      <p>{props.item.description}</p>
+    </div>
+  }
+
+  if (props.item.itemURL) {
+    itemURL = <ReactTinyLink
+        cardSize="small"
+        showGraphic={true}
+        maxLine={3}
+        minLine={1}
+        url={props.item.itemURL}
+    />
+  }
+
+  const EditButton = () => (
+    <button type="button" className="btn btn-primary" id="event-edit-button" onClick={(event) => props.onEditButtonClick(props.item)}>
+      <strong>Edit Wishlist Item</strong>
+    </button>
+  )
 
   return (
     <Card className={classes.root}>
       <CardActionArea>
-        <CardMedia
-          className={classes.media}
-          image="https://images.unsplash.com/photo-1540573133985-87b6da6d54a9?ixlib=rb-1.2.1&auto=format&fit=crop&w=1955&q=80"
-          title="Surprised monkey"
-        />
+        {itemURL}
         <CardContent>
           <Typography gutterBottom variant="h5" component="h2" className={classes.title}>
-            OMG it's a Monkey!
+            {props.item.item}
           </Typography>
           <Typography variant="body2" color="textSecondary" component="p">
-          Monkey is a common name that may refer to groups or species of mammals.
-          The term is applied descriptively to groups of primates.
+            {description}
           </Typography>
         </CardContent>
       </CardActionArea>
       <CardActions>
-        <Button size="small" color="primary" href="https://unsplash.com/photos/Z05GiksmqYU">
-          See it on Unsplash
-        </Button>
+        <EditButton/>
       </CardActions>
     </Card>
   )
-}
+})
 
-export default Item
+export default ItemContainer;

24096ff1f3133d09970bb6bb2b05d54de5233871
Ariana Bray <aribray@gmail.com>
2020-11-30 21:46:19 -0800

    add icon credit

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -31,6 +31,9 @@ const NavigationAuth = () => (
       <li>
         <Button type="button" className="btn btn-info" href={ROUTES.WISHLIST}>Wish List</Button>
       </li>
+      {/* <li> */}
+        {/* <Button type="button" className="btn btn-info" href={ROUTES.PHOTOS}>Photo Gallery</Button> */}
+      {/* </li> */}
       <li>
         <SignOutButton />
       </li>
@@ -39,6 +42,7 @@ const NavigationAuth = () => (
 )
 
   const NavigationNonAuth = () => (
+  <div>
     <ul className="nav nav-pills">
       <li>
         <Button type="button" className="btn btn-primary" href={ROUTES.LANDING}>Landing</Button>
@@ -47,55 +51,10 @@ const NavigationAuth = () => (
         <Button type="button" className="btn btn-primary" href={ROUTES.SIGN_IN}>Sign In</Button>
       </li>
     </ul>
+  <div>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+  </div>
+
   );
-
-// const NavigationAuth = ( {calendarStore, wishlistStore } ) => (
-//   <div>
-//     <Navbar bg="primary" expand="lg" variant="dark">
-//         <Navbar.Brand href="#home">A Very Virtual Christmas</Navbar.Brand>
-//         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-//         <Navbar.Collapse id="basic-navbar-nav">
-//         <Nav className="mr-auto">
-//             <Nav.Button href={ROUTES.LANDING}>Home</Nav.Button>
-//             <Nav.Button href={ROUTES.WISHLIST}>Wishlist</Nav.Button>
-//             <Nav.Button href={ROUTES.CALENDAR}>Calendar</Nav.Button>
-//         </Nav>
-//         </Navbar.Collapse>
-//         </Navbar>
-//         <Route
-//         path={ROUTES.CALENDAR}
-//         component={ props => <HomePage {...props} calendarStore={calendarStore} />}
-//         // render={props => <HomePage {...props} calendarStore={calendarStore} />}
-//         />
-//         <Route
-//         path={ROUTES.WISHLIST}
-//         component={props => <Wishlist {...props} wishlistStore={wishlistStore} />}
-//         // render={(props) => (
-//         // <Wishlist {...props} wishlistStore={wishlistStore} />
-//         // )}
-//     />
-//   </div>
-// );
-
-// const NavigationNonAuth = () => (
-//         <div>
-//     <Navbar bg="primary" expand="lg" variant="dark">
-//         <Navbar.Brand href="#home">A Very Virtual Christmas</Navbar.Brand>
-//         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-//         <Navbar.Collapse id="basic-navbar-nav">
-//         <Nav className="mr-auto">
-//             <Nav.Button href={ROUTES.LANDING}>Home</Nav.Button>
-//             <Nav.Button href={ROUTES.SIGN_IN}>Sign In</Nav.Button>
-//         </Nav>
-//         </Navbar.Collapse>
-//         </Navbar>
-//         <Route
-//         path={ROUTES.SIGN_IN}
-//         exact
-//         render={props => <SignInPage {...props} />}
-//         />
-//   </div>
-// )
 
 export default Navigation;
 
